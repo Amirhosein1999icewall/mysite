503840ebb13486daef13962904a41a6bec27cdb6
Amirhosein1999icewall <amirhoseinahmadi1999aacc@gmail.com>
2026-06-02 21:50:12 +0330

    add general template file

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return render (request,'page_o/index.html')
+    return render (request,'website/index.html')
 
 def contact_view(request):
-    return render (request,'page_o/contact.html')
+    return render (request,'website/contact.html')
 
 def about_view(request):
-    return render (request,'page_o/about.html')
+    return render (request,'website/about.html')
